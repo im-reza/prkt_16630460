534c468b88a01adc34c4607d468cc6ade2b5a79f
mreza18 <reza18sep@gmail.com>
2020-07-02 07:06:03 +0700

    tugas praktikum MuhammadReza 16630460

--- application/controllers/laporan/Barang.php
+++ application/controllers/laporan/Barang.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
 
--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Dashboard Administrator</h1>
+                <h1 class="page-header">Hello, <?= $nama; ?></h1>
             </div>
             <!-- /.col-lg-12 -->
         </div>
